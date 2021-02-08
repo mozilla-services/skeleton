d63a7269766a893597ea77a6f0615143d373ef65
jrconlin <jr+git@mozilla.com>
2021-02-08 13:58:32 -0800

    chore: Update dependencies
    * Update tools/rust_setup.sh to not use nightly.
    * Added "Your Name Here" comment for Cargo.toml
    * Add rust-toolchain file to set the default toolchain for the project
    
    Issue #1

--- tools/rust_setup.sh
+++ tools/rust_setup.sh
@@ -5,11 +5,16 @@ then
     echo "rustup not found. Please either run 'rustup_install.sh' or 'curl https://sh.rustup.rs -sSf | sh]'"
     return
 fi
-# Fetch rustup if not installed.
+## Fetch rustup if not installed.
 # curl https://sh.rustup.rs -sSf | sh
-rustup install nightly
-rustup update nightly
-# if you want clippy, the rust linter:
-#cargo +nightly install clippy
-# if you want to use rust-wasm, uncomment
+## CHANNEL is the default channel for this project ('stable', 'nightly', etc.)
+## You only need the following if you're planning on using a non-stable channel
+#$CHANNEL = stable
+#rustup install $CHANNEL
+#rustup update $CHANNEL
+## use Nightly with this project
+#rustup override set $CHANNEL
+## if you want clippy, the rust linter:
+# cargo component add clippy-preview
+## if you want to use rust-wasm, uncomment
 #cargo install -f cargo-web
